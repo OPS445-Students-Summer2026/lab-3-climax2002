1285d867551d71c06d1d5980ef7eaf5fd8bf8e4c
Clinton Folarin <ctfolarin@myseneca.ca>
2026-05-22 15:35:07 -0400

    all lab3

---
+++ lab3d.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# OPS445 - Lab 3
+# lab3d.py
+# Author: Clinton folarin
+
+import subprocess
+
+def free_space():
+	process = subprocess.Popen(["df -h | grep '/$' | awk '{print $4}'"],
+								stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+								stderr=subprocess.PIPE, shell=True)
+	output = process.communicate()
+	return output[0].decode('utf-8').strip()
+	
+if __name__ == '__main__':
+	print(free_space())
