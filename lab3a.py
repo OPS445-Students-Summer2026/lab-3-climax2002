81b8ffb60758de43682c085d4f65e7ec3f09e67b
Clinton Folarin <ctfolarin@myseneca.ca>
2026-05-22 14:10:09 -0400

    Lab 3 Done

---
+++ lab3a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# OPS445 - Lab 3
+# lab3a.py
+# Author: Clinton folarin
+
+# return_text_value() function
+
+def return_text_value():
+	name = 'Terry'
+	greeting = 'Good Morning ' + name
+	return greeting
+
+# return_number_value() function
+
+def return_number_value():
+	num1 = 10
+	num2 = 5
+	num3 = num1 + num2
+	return num3
+
+# Main program
+if __name__ == '__main__':
+	print('python code')
+	text = return_text_value()
+	print(text)
+	number = return_number_value()
+	print(str(number))
